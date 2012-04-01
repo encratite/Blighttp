857aebd0ac90bbe9b38aafd65f199b0bcc51482a
void <git.email.address@googlemail.com>
2012-04-01 16:11:29 +0200

    Switched to the new automatic assembly info generation system

---
+++ Blighttp/Properties/AssemblyInfo.template.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Blighttp")]
+[assembly: AssemblyDescription("Dynamic web content framework")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("c769132a-7b92-471b-8b7d-50196a8fbb62")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
---
+++ Test/Properties/AssemblyInfo.template.cs
@@ -0,0 +1,18 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Test")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("")]
+[assembly: AssemblyCopyright("")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("79cbc1b9-4c97-43a2-97b4-cc1426370a02")]
+
+[assembly: AssemblyVersion("0.0.0.0")]
